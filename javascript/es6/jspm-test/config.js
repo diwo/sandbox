9982265f28b54d9112f5ea9ae7504d8761ddd5fa
Dickson Wong <dicksonwong@gmail.com>
2015-01-14 18:41:24 -0800

    Trying out handlebars

--- javascript/es6/jspm-test/config.js
+++ javascript/es6/jspm-test/config.js
@@ -10,9 +10,17 @@ System.config({
 System.config({
   "map": {
     "assert": "github:jspm/nodelibs-assert@0.1.0",
+    "handlebars": "npm:handlebars@2.0.0",
     "interface-verify": "github:diwo/interface-verify@master",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-fs@0.1.0": {
+      "assert": "npm:assert@1.3.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.0": {
       "process": "npm:process@0.10.0"
@@ -20,15 +28,60 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
+    "npm:amdefine@0.1.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
+    },
+    "npm:async@0.2.10": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
+    "npm:handlebars@2.0.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "optimist": "npm:optimist@0.3.7",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "uglify-js": "npm:uglify-js@2.3.6"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:optimist@0.3.7": {
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "wordwrap": "npm:wordwrap@0.0.2"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
+    "npm:source-map@0.1.43": {
+      "amdefine": "npm:amdefine@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
+    "npm:uglify-js@2.3.6": {
+      "async": "npm:async@0.2.10",
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "optimist": "npm:optimist@0.3.7",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "source-map": "npm:source-map@0.1.43",
+      "util": "github:jspm/nodelibs-util@0.1.0",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
+    },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.0"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
--- javascript/es6/jspm-test/lib/main.js
+++ javascript/es6/jspm-test/lib/main.js
@@ -3,8 +3,43 @@
 import interfaceVerify from 'interface-verify';
 import assert from 'assert';
 import hello from './hello';
+import Handlebars from 'handlebars';
 
 assert.equal(typeof interfaceVerify, 'object');
 
 var textNode = document.createTextNode(`${hello} world`);
 document.getElementById('main').appendChild(textNode);
+
+var divNode = document.createElement('div');
+divNode.innerHTML =
+  Handlebars.compile([
+    '<p>Hello my name is {{name}}.</p>',
+    'Some of my favorite things are:',
+    '<ul>{{#fav}}<li>{{thing}}</li>{{/fav}}</ul>'
+  ].join(''))({
+    name: 'Dickson',
+    fav: [
+      {thing: 'food'},
+      {thing: 'sleep'},
+      {thing: 'good food'}
+    ]
+  });
+document.getElementById('main').appendChild(divNode);
+
+Handlebars.registerPartial('inception',
+  '{{#with dream}}<span style="font-size: 0.8em;">dreaming about being in a {{loc}}... {{> inception}}</span>{{/with}}');
+var anotherNode = document.createElement('div');
+anotherNode.innerHTML =
+  Handlebars.compile('I was in a {{loc}}... {{> inception}}')({
+    loc: 'plane',
+    dream: {
+      loc: 'car',
+      dream: {
+        loc: 'hotel',
+        dream: {
+          loc: 'maze'
+        }
+      }
+    }
+  });
+document.getElementById('main').appendChild(anotherNode);
